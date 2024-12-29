557645b9959596d26a1e004f541850561cf8c862
Kumudu Mohottala <kumudulaksitha@gmail.com>
2024-12-29 16:43:10 +0530

    removed unused snippets

--- data/scripts/dota/create_dataset_from_videos.py
+++ data/scripts/dota/create_dataset_from_videos.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 import argparse
 import os
 from sklearn.model_selection import train_test_split
